334e1553f94eb101c7502ec497db6c75e99ea963
jesusninoc <jesusmnc@hotmail.es>
2015-12-16 15:32:53 +0100

    Create EjercicioFicherosVocalesConsonantes.ps1

---
+++ EjercicioFicherosVocalesConsonantes.ps1
@@ -0,0 +1,6 @@
+#De una lista de ficheros que tienen una letra y la extensión TXT
+#Ejemplo:
+#a.txt
+#b.txt
+#c.txt
+#Mover los ficheros que tengan vocales a la carpeta vocales y los que no sean vocales a la carpeta consonantes
