36488a06db4410b7f51f644e3b31861630d2c689
jesusninoc <jesusmnc@hotmail.es>
2015-12-16 15:15:42 +0100

    Create Examen08-10-2015Enunciado.ps1

---
+++ Examen08-10-2015Enunciado.ps1
@@ -0,0 +1,12 @@
+#Listar los procesos con varios alias
+#Listar los servicios con varios alias
+#Mostrar los 5 primeros
+#Mostrar los 5 últimos
+#Ordenar por CPU descendente
+#Añadir a una variable el resultado de ordenar por CPU descendente
+#Agrupar por nombre de proceso y guardar el resultado en una variable
+#Mostrar los ficheros que tienen más de 1000 bytes (where-object, get-help about_comparison_operators)
+
+#¿Qué hacen los siguientes scripts?
+#Get-Process | sort-object CPU | Write-Host -foregroundcolor blue -backgroundcolor red 
+#Write-Host (2,4,6,8,10,12) -Separator ", -> " -foregroundcolor DarkGreen -backgroundcolor white
