72331f9e428d08e4aa477b991a42f6c061df9706
jesusninoc <jesusmnc@hotmail.es>
2015-12-16 23:36:23 +0100

    Create Examen16-12-2015Enunciado.ps1

---
+++ Examen16-12-2015Enunciado.ps1
@@ -0,0 +1,11 @@
+#Del siguiente listado de ficheros:
+
+#PS F:\power\fichero> ls
+#Directorio: F:\power\fichero
+#Mode        LastWriteTime     	      Length Name
+-a---        15/12/2014     22:24          8 a1.txt
+-a---        15/12/2014     22:24          8 b.txt      
+
+#Realizar las siguientes tareas:
+#1. Si el fichero tiene una vocal y el número 1 al lado, se mueve el fichero a la carpeta vocal y si es una letra no vocal se mueve a la carpeta novocal.
+#2. Realizar el mismo ejercicio pero en el caso de tener un 1 el nombre, el nombre que se guarda en la carpeta es sin el 1, es decir en lugar de a1.txt se mueve a la carpeta correspondiente con el nuevo nombre que es a.txt.
