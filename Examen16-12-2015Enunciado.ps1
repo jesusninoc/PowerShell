3bf6e7f75d1425c8cb60b302b2b621de91cbb00b
jesusninoc <jesusmnc@hotmail.es>
2015-12-16 23:37:19 +0100

    Update Examen16-12-2015Enunciado.ps1

--- Examen16-12-2015Enunciado.ps1
+++ Examen16-12-2015Enunciado.ps1
@@ -3,8 +3,8 @@
 #PS F:\power\fichero> ls
 #Directorio: F:\power\fichero
 #Mode        LastWriteTime     	      Length Name
--a---        15/12/2014     22:24          8 a1.txt
--a---        15/12/2014     22:24          8 b.txt      
+#-a---        15/12/2014     22:24          8 a1.txt
+#-a---        15/12/2014     22:24          8 b.txt      
 
 #Realizar las siguientes tareas:
 #1. Si el fichero tiene una vocal y el número 1 al lado, se mueve el fichero a la carpeta vocal y si es una letra no vocal se mueve a la carpeta novocal.
