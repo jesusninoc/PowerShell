a6f236893a159def24170bdf9598bd4313791cfd
jesusninoc <jesusmnc@hotmail.es>
2015-12-16 10:50:31 +0100

    Repaso bucle ForEach

---
+++ RepasoBucleForEach.ps1
@@ -0,0 +1,63 @@
+﻿$numeros='1','2','3'
+$numeros | %{$_}
+$numeros | %{$PSItem}
+foreach($numero in $numeros)
+{
+$numero
+}
+
+$numeros=1,2,3
+$numeros | %{$PSItem}
+$numeros | %{$_}
+foreach($numero in $numeros)
+{
+$numero
+}
+
+$numeros=1..3
+$numeros | %{$PSItem}
+$numeros | %{$_}
+foreach($numero in $numeros)
+{
+$numero
+}
+
+$numeros='1','2','3'
+$numeros | %{$PSItem}
+$numeros | %{$_}
+foreach($numero in $numeros)
+{
+$numeros
+}
+
+$numeros='1''2''3'
+$numeros | %{$PSItem}
+$numeros | %{$_}
+foreach($numero in $numeros)
+{
+$numero
+}
+
+$numeros='1,2,3'
+$numeros | %{$PSItem}
+$numeros | %{$_}
+foreach($numero in $numeros)
+{
+$numero
+}
+
+$numeros='1''2''3'
+$numeros | %{$PSItem}
+$numeros | %{$_}
+foreach($numero in $numeros)
+{
+$numeros
+}
+
+$numeros="'1','2','3'"
+$numeros | %{$PSItem}
+$numeros | %{$_}
+foreach($numero in $numeros)
+{
+$numeros
+}
