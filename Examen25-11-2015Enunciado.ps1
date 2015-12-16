58e7de70c23437a82f0397abe67f2d4a64e48e05
jesusninoc <jesusmnc@hotmail.es>
2015-12-16 15:35:33 +0100

    Create Examen25-11-2015Enunciado.ps1

---
+++ Examen25-11-2015Enunciado.ps1
@@ -0,0 +1,13 @@
+#1- Obtener los cinco primeros procesos cuyo consumo de CPU sea mayor y guardarlo en un fichero, ese fichero tiene que estar dentro de una carpeta cuyo nombre es la fecha de hoy, cada vez que se ejecuta se tiene que añadir la información al fichero
+#Sacar los cinco primeros procesos
+#Ordenados por mayor consumo de CPU a menos consumo
+#Mostrar solo los 5 primeros procesos
+
+#2- Obtener los cinco primeros procesos cuyo consumo de CPU sea mayor y guardarlo en un fichero, ese fichero tiene que estar dentro de una carpeta cuyo nombre es la fecha de hoy, cada vez que se ejecuta se tiene que crear un nuevo fichero
+
+#3- Obtener los cinco primeros procesos cuyo consumo de CPU sea mayor y guardar para cada uno de los procesos un fichero con la información:
+#NPM(K): The amount of non-paged memory that the process is using, in kilobytes.
+#PM(K): The amount of pageable memory that the process is using, in kilobytes.
+#WS(K): The size of the working set of the process, in kilobytes. The working set consists of the pages of memory that were recently referenced by the process.
+#VM(M): The amount of virtual memory that the process is using, in megabytes. Virtual memory includes storage in the paging files on disk.
+#CPU(s): The amount of processor time that the process has used on all processors, in seconds.
