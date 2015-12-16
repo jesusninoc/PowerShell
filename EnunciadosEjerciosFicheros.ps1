7a6a2a3aa5e29c4a119cd58a1fe45dc9a2b693c3
jesusninoc <jesusmnc@hotmail.es>
2015-12-16 16:01:18 +0100

    Update and rename EnunciadosEjerciosFicheros.ps1 to EnunciadosEjerciosFicherosProcesos.ps1

--- EnunciadosEjerciosFicheros.ps1
+++
@@ -1,6 +0,0 @@
-#Crear un directorio para cada uno de los usuarios: Juan, Marta, Pedro, Carlos
-#Crear un fichero en cada uno de los directorios creados anteriormente
-#Añadir contenido a cada uno de los ficheros, en concreto los procesos que se están ejecutando en este mismo momento
-#Copiar cada uno de los directorios en una carpeta nueva añadiendo un 2 a cada nombre de directorio: Juan2, Marta2, Pedro2, Carlos2
-#Eliminar la carpeta nueva cuyo contenido empiza por J (recurse)
-#Mover las carpetas creadas al principio en una carpeta nueva que se llame Copias
---
+++ EnunciadosEjerciosFicherosProcesos.ps1
@@ -0,0 +1,40 @@
+#Crear un directorio para cada uno de los usuarios: Juan, Marta, Pedro, Carlos
+#Crear un fichero en cada uno de los directorios creados anteriormente
+#Añadir contenido a cada uno de los ficheros, en concreto los procesos que se están ejecutando en este mismo momento
+#Copiar cada uno de los directorios en una carpeta nueva añadiendo un 2 a cada nombre de directorio: Juan2, Marta2, Pedro2, Carlos2
+#Eliminar la carpeta nueva cuyo contenido empiza por J (recurse)
+#Mover las carpetas creadas al principio en una carpeta nueva que se llame Copias
+
+#Realizar copias de seguridad de varios alumnos (tienes que simular el escenario)
+#Separar en tres grupos los ficheros que sean 0, pares o impares, los fichero se han creado siguiendo la siguiente serie: 0, 1, 2, 3, 4,… 
+#Separar archivos si el fichero empiza por a y moverlos a una carpeta que se llama A, lo mismo para la letra b y la letra c
+#Separar los procesos que consuman más de 10 megas moviendo dichos proceos a la carpeta 10m, lo mismo para los que consuman más de 20 megas
+#Mover los ficheros que hayan sido creados la semana pasada a una carpeta que sea semanapasada
+
+#Preguntas cortas:
+#Get-ChildItem sirve para listar ficheros y carpetas, nombrar otras formas de listar ficheros y carpetas
+#Listar todos los ficheros y carpetas recursivamente
+#Listar todos los ficheros, pero sólo el nombre
+#Listar los ficheros ocultos
+#Listar los ficheros que tengan la extensión .log
+#Listar los ficheros sin las extensiones .dll
+#Crear un directorio para cada usuario
+#Crear un fichero para cada usuario
+#Añadir contenido a cada fichero creado para cada usuario
+#Elimiar un directorio
+#Renombrar un directorio
+#Mover archivos de una carpeta a otra
+#Si el archivo que se quiere eliminar pertenece a un usuario "complicado" no se elimina
+#Asignar permisos para una carpeta para cada usuario
+#Asignar permisos para un fichero para cada usuario
+#Asignar permisos para una carpeta para cada grupo
+#Asignar permisos para un fichero para cada grupo
+#Arrancar un proceso o un servicio
+#Eliminar un proceso o un servicio
+#Crear una tarea programada
+#Iniciar un proceso siendo un usuario en concreto
+#Crear un usuario
+#Crear un grupo
+#Añadir un usuario a un grupo
+#Indicar si se está ejecutando o no un proceso
+#Indicar si se está ejecutando o no un servicio
