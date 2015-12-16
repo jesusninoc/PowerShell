564aa1e0069240d0d3202c9bd2c131623a1f653e
jesusninoc <jesusmnc@hotmail.es>
2015-12-16 15:48:41 +0100

    Create EjemplosFicheros.ps1

---
+++ EjemplosFicheros.ps1
@@ -0,0 +1,6 @@
+Get-ChildItem
+Get-ChildItem –Path *.txt -Recurse -Force
+Get-ChildItem -Name
+Get-ChildItem –Path C:\Windows\Logs\* -Include *.txt -Exclude A*
+Get-ChildItem –Path HKLM:\Software
+Get-ChildItem –Path C:\Windows –Include *mouse* -Exclude *.png
