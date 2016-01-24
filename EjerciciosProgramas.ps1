95587c8b719609bdeb5a46dd60ce7cd147e62472
jesusninoc <jesusmnc@hotmail.es>
2016-01-24 13:07:07 +0100

    Update EjerciciosProgramas.ps1

--- EjerciciosProgramas.ps1
+++ EjerciciosProgramas.ps1
@@ -1,3 +1,28 @@
+#Número de programas instalados (count)
+((Get-WmiObject -Class Win32_Product).name).count
+
+#Selecciona el nombre y versión de los programas instalados
+(Get-WmiObject -Class Win32_Product) | Select-Object name,vendor
+
+#Dime la versión de los programas instalados
+(Get-WmiObject -Class Win32_Product).version
+(Get-WmiObject -Class Win32_Product) | Select-Object name,version
+
+#Agrupa los programas cuyo nombre sea "Microsoft Office Proof (Spanish) 2007"
+(Get-WmiObject -Class Win32_Product) | Select-String "MySQL" | Group-Object
+
+#Dime si está instalado Gimp
+if(((Get-WmiObject -Class Win32_Product).name) | Select-String "Gimp"){"Instalado"}
+
+#Agrupa los progrmas cuyo vendedor sea Microsoft
+(Get-WmiObject -Class Win32_Product).vendor | Select-String "Microsoft" | Group-Object
+
+#Dime el número de programas cuyo nombre sea Microsoft (agrupar)
+((Get-WmiObject -Class Win32_Product).vendor | Select-String "Microsoft" | Group-Object).Count
+
+#Dime si está instalado Geany
+if(((Get-WmiObject -Class Win32_Product).name) | Select-String "Geany"){"Instalado"}
+
 #Listar programas instalados
 (Get-WmiObject -Class Win32_Product).name
 
