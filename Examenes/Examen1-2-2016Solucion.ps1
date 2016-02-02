7e160470d8011c6f8f3ba07f86b468489adfb88b
jesusninoc <jesusmnc@hotmail.es>
2016-02-03 00:09:37 +0100

    Update Examen1-2-2016Solucion.ps1

--- Examenes/Examen1-2-2016Solucion.ps1
+++ Examenes/Examen1-2-2016Solucion.ps1
@@ -35,10 +35,3 @@ $valors1
 Start-Sleep -Seconds 5
 $valors2 | Out-File nombreproducto.txt
 gc nombreproducto.txt
-
-#----------
-#Linux
-#----------
-#Crear un script en el que pida un nombre al usuario y si el nombre es 'pepito' que almacene dicho nombre en un fichero.
-
-#Recorrer un fichero que tiene nombres de personas y si la persona que está dentro del fichero es 'pepito' almacenar dicho nombre en un fichero.
