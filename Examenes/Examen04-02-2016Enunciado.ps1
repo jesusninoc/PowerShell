8631898d34640cc08c9e419f5a953e8b244b2feb
jesusninoc <jesusmnc@hotmail.es>
2016-02-04 22:47:58 +0100

    Update and rename Examen04-02-2016Enunciado.sh to Examen04-02-2016Enunciado.ps1

---
+++ Examenes/Examen04-02-2016Enunciado.ps1
@@ -0,0 +1,1 @@
+#Realizar un programa que pida al usuario un proceso y almacene el nombre de todos los ficheros dll de dicho proceso junto con su hash en un fichero.
--- Examenes/Examen04-02-2016Enunciado.sh
+++
@@ -1,1 +0,0 @@
-Realizar un programa que pida al usuario un proceso y almacene el nombre de todos los ficheros dll de dicho proceso junto con su hash en un fichero.
