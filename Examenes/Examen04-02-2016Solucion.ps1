127bf1db357b89abbd8f53a514f23972bb8037b9
jesusninoc <jesusmnc@hotmail.es>
2016-02-04 22:52:26 +0100

    Update Examen04-02-2016Solucion.ps1

--- Examenes/Examen04-02-2016Solucion.ps1
+++ Examenes/Examen04-02-2016Solucion.ps1
@@ -1,3 +1,4 @@
 #Realizar un programa que pida al usuario un proceso y almacene el nombre de todos los ficheros dll de dicho proceso junto con su hash en un fichero.
 
-
+$programa=Read-Host "Proceso del que obtener las DLL"
+Get-Process -Name $programa -Module | % {(Get-FileHash $_.FileName -Algorithm SHA1) | Out-File D:\power\dllhash.txt -Append}
