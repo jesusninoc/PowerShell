52509600433df5e49892cbe9e172e5bf0999977b
jesusninoc <jesusmnc@hotmail.es>
2016-02-04 22:46:05 +0100

    Update Examen04-02-2016Enunciado.sh

--- Examenes/Examen04-02-2016Enunciado.sh
+++ Examenes/Examen04-02-2016Enunciado.sh
@@ -1,1 +1,1 @@
-Realizar un programa que pida al usuario un proceso y almacene todas las dll de dicho proceso junto con su hash en un fichero.
+Realizar un programa que pida al usuario un proceso y almacene el nombre de todos los ficheros dll de dicho proceso junto con su hash en un fichero.
