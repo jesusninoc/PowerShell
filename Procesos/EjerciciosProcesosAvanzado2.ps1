770026c66289f8625407acaa7c71b30aac150f22
jesusninoc <jesusmnc@hotmail.es>
2016-02-17 14:24:36 +0100

    Create EjerciciosProcesosAvanzado2.ps1

---
+++ Procesos/EjerciciosProcesosAvanzado2.ps1
@@ -0,0 +1,5 @@
+#Listar procesos, crear carpeta para cada uno y dentro de la carpeta meter un fichero con el hash del proceso (necesario saber la ruta absoluta del programa).
+#Si el nombre de un proceso es nano almacenar información completa sobre dicho proceso ( | select *) y realizar un hash, almacenar el hash.
+#Listar procesos, ver la ruta del programa y calcular hash para cada programa.
+#Almacenar en un fichero información sobre los programas instalados y las actualizaciones.
+#Guardar dicha información en una carpeta con la fecha actual.
