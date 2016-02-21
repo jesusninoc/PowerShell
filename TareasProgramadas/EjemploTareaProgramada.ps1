1cfad1a5c317ca9c0f483dcebd696259bbb2d60d
jesusninoc <jesusmnc@hotmail.es>
2016-02-21 22:23:45 +0100

    Update EjemploTareaProgramada.ps1

--- TareasProgramadas/EjemploTareaProgramada.ps1
+++ TareasProgramadas/EjemploTareaProgramada.ps1
@@ -2,3 +2,6 @@
 $action=New-ScheduledTaskAction -Execute 'Powershell.exe' -Argument '-NoProfile -WindowStyle Hidden -command D:\mysqlmail.ps1'
 $trigger=New-ScheduledTaskTrigger -Daily -At 9am
 Register-ScheduledTask -Action $action -Trigger $trigger -TaskName "TareaProgramada" -Description "Comprobación de valor"
+
+#Crear una tarea programada que ejecuta Notepa
+Register-ScheduledTask Task01 -Action (New-ScheduledTaskAction -Execute "notepad") -Principal (New-ScheduledTaskPrincipal -GroupId "BUILTIN\administradores" -RunLevel Highest) -Settings (New-ScheduledTaskSettingsSet -RestartCount 5 -RestartInterval 60)
