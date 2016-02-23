042859d225995ad5e35dbdbff7c06fe142515409
jesusninoc <jesusmnc@hotmail.es>
2016-02-23 01:12:33 +0100

    Update EjemplosRed.ps1

--- Red/EjemplosRed.ps1
+++ Red/EjemplosRed.ps1
@@ -6,6 +6,16 @@ gc .\ips.txt | %{Test-Connection $_}
 1..254 | %{ping ('192.168.1.'+$_)}
 1..254 | %{Test-Connection ('192.168.1.'+$_)}
 
+#Realizar ping a las direcciones IP de un rango variando el tercer y cuarto octeto
+foreach($tercero in 1..254)
+{
+    foreach($cuarto in 1..254)
+    {
+        $ip='192.168.'+$tercero+'.'+$cuarto
+        ping $ip
+    }
+}
+
 #Apagar equipos remotos
 foreach ($equipo in Get-Content "F:\pcs.txt"){(Get-WmiObject -class win32_operatingsystem -computername $equipo).win32shutdown(12)}
 
