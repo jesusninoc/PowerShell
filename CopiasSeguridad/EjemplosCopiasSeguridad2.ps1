bd2e0c9150d94bb9484c830c49652707135e4d54
jesusninoc <jesusninoc@users.noreply.github.com>
2016-04-18 23:21:28 +0200

    Introducción a la copia de seguridad diferencial

---
+++ CopiasSeguridad/EjemplosCopiasSeguridad2.ps1
@@ -0,0 +1,10 @@
+#Simular copia diferencial
+$valor1="hola"
+$valor2="hola a"
+$valor3="hola ad"
+$valor4="hola adi"
+$valor5="hola adio"
+$valor6="hola adios"
+
+$xi=$valor4.Replace($valor1,"");$xi
+$vi=$valor6.Replace($valor1,"");$vi
