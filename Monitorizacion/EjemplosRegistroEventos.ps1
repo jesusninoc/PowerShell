5983a00ab3c3922c205a8b51d4946333ca4f1ea3
jesusninoc <jesusninoc@users.noreply.github.com>
2016-05-03 22:22:30 +0200

    Ejemplos registro eventos

---
+++ Monitorizacion/EjemplosRegistroEventos.ps1
@@ -0,0 +1,31 @@
+#Eventos
+
+#Eventos disponibles
+Get-EventLog -List
+
+#Información sobre log
+Get-EventLog Application
+Get-EventLog HardwareEvents
+Get-EventLog "Internet Explorer"
+Get-EventLog Security
+Get-EventLog System
+Get-EventLog "Windows PowerShell"
+
+#Agrupar mensajes del log Application
+Get-EventLog Application | Group-Object Message
+Get-EventLog Application | Group-Object Message | Select-Object Name
+Get-EventLog Application | Group-Object Message | Select-Object Name | Format-Custom
+
+#Información sobre el mensaje "gupdate" OJO!!
+Get-EventLog Application | Where-Object Message -Match "gupdate" | Select-Object Message | Format-Custom
+
+#Información sobre el mensaje "gpupdate"
+Get-EventLog Application | Where-Object Message -Match "guppdate" | Select-Object Message | Format-Custom
+
+#Información sobre IP en los logs
+Get-EventLog Application | Where-Object Message -Match "192.168"
+Get-EventLog Application | Where-Object Message -Match "192.168" | Select-Object Message
+Get-EventLog Application | Where-Object Message -Match "192.168" | Select-Object Message | Format-Custom
+
+#Información sobre el mensaje "explore", analizar el proceso Explore
+Get-EventLog Application | Where-Object Message -Match "explore" | Select-Object Message | Format-Custom
