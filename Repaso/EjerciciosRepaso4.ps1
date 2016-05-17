d855adc59cd72c0d8e47d511da62ff13134af69f
jesusninoc <jesusninoc@users.noreply.github.com>
2016-05-17 23:25:03 +0200

    Repaso Red

---
+++ Repaso/EjerciciosRepaso4.ps1
@@ -0,0 +1,23 @@
+#Escanear IP creado por Edu
+
+[int]$rangoip= read-host "Rango de dirección IP a escanear (1-254)"
+1 .. 254 | %{$_
+ $ip="192.168.$rangoip."+$_
+ $resultadoping = Resolve-DnsName $ip -ErrorAction Ignore
+ 
+    if ($resultadoping.Type -eq "PTR")
+        {
+        "$ip" + "--> existe"
+        "$ip"+ "-->" ,($resultadoping).NameHost >> Q:\direccionesip.txt
+        }
+   else 
+    {
+    "$ip"+"--> no existe, haciendo ping..."
+    $ping2=ping $ip
+        if($ping2 -match "TTL")
+        {
+            "$ip"+"--> si existe" 
+            "$ip"+ "-->" >> Q:\direccionesip.txt
+        }
+    }
+ }
