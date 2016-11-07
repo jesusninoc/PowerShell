1eb8ee6a292b4ec3789ed96bec2e76e287acdb9b
jesusninoc <jesusninoc@users.noreply.github.com>
2016-11-07 23:20:17 +0100

    Examen 14-11-2017

---
+++ Examenes/Examen14-11-2017Enunciado.ps1
@@ -0,0 +1,30 @@
+#TEN EN CUENTA QUE SABES HACER SCRIPTS EN POWERSHELL Y EN LINUX
+#TIENES CONOCIMIENTOS SOBRE: HARDWARE Y SOFTWARE, SISTEMAS OPERATIVOS (MEMORIA, PROCESOS, ARCHIVOS, ENTRADA Y SALIDA), VIRTUALIZACIÓN E INSTALACIÓN DE SISTEMAS OPERATIVOS.	
+
+#¿Qué recurso es el más valorado dentro del ordenador porque es escaso?. HAZ UN INFORME SOBRE TODO LO VEAS QUE ESTÁ RELACIONADO CON EL RECURSO MÁS VALORADO.
+
+#¿Qué problema presenta el procesador en relación con otros componentes más del ordenador?. HAZ UN INFORME SOBRE LA RELACIÓN DEL PROCESADOR CON OTROS COMPONENTES.
+
+#En el laboratorio 3 (vdi) podemos hacer la siguiente pregunta: ¿el ordenador no funciona?, ¿es correcto?, explica los motivos por los que piensas que es correcto o que no lo es. ¿CÓMO DETECTAS UN PROBLEMA EN UN EQUIPO VIRTUAL?.
+
+#¿Qué elementos tiene un ordenador?, ¿qué papel juega el sistema operativo en el ordenador?, responde a las preguntas y explica qué es el sistema operativo. HAZ UN INFORME SOBRE TODOS LOS ELEMENTOS QUE CONTIENE UN ORDENADOR.
+
+#Si un empleado utiliza un programa que ya no funciona en las versión de Windows 10, ¿qué harías para pudiera trabajar el empleado?. Explica en detalle la solución. ¿CÓMO PUEDES SABER QUÉ PROGRAMAS UTILIZAN LOS EMPLEADOS?, ¿PUEDES SABER SI FUNCIONA O NO FUNCIONA EN EL NUEVO WINDOWS?.
+
+#¿Qué procedimiento utilizas para instalar sistemas operativos en un entorno virtual?, explica el proceso. ¿QUÉ SABES CREAR UTILIZANDO COMANDOS EN ENTORNO VIRTUAL?.
+
+#¿Qué ha podido ocurrir?, soluciona el siguiente caso: se ha producido una fuga de información dentro de la empresa, un empleado ha logrado robar 1 GB de información y no ha sido detectado por ningún elemento de la red. ¿CÓMO HARÍAS ESTO EN POWERSHELL?, ¿CÓMO PUEDES MONITORIZAR LA FUGA POR LA RED?, ¿PUEDES HACER ALGÚN SCRIPT PARA DETECTAR UNA FUGA DENTRO DEL SISTEMA OPERATIVO DE CADA EMPLEADO?.
+
+#¿Cómo puedes darte cuenta de que hay un proceso que consume toda la memoria RAM del equipo?. EXPLICA LOS CMDLETS QUE UTILIZAS.
+
+#DIME EN QUÉ RUTA ESTÁ EL PROGRAMA QUE SE ESTÁ EJECUTANDO (LINUX Y WINDOWS).
+
+#HACER UN LOGIN Y ALMACENAR EN UN FICHERO LOS INTENTOS DE NOMBRE DE USUARIO Y CONTRASEÑA.
+
+#A UN ALUMNO CADA VEZ QUE ESCRIBE "OUT-FIL" LE APARECEN EN LA UNIDAD Q:\ 100 ARCHIVOS, ¿QUÉ LE ESTÁ PASANDO?, ¿CÓMO HAN PODIDO HACER ESO?.
+
+#MOSTRAR LOS PROCESOS QUE PRESENTAN COMPORTAMIENTOS EXTRAÑOS (MUCHO CONSUMO DE CUALQUIER PARÁMETRO)
+#PARA NIVEL AVANZADO... PUEDES ANALIZAR TAMBIÉN HILOS.
+
+#MOSTRAR LOS SERVICIOS QUE PRESENTAN COMPORTAMIENTOS EXTRAÑOS (MUCHO CONSUMO DE CUALQUIER PARÁMETRO)
+#PARA NIVEL AVANZADO... PUEDES ANALIZAR TAMBIÉN HILOS.
