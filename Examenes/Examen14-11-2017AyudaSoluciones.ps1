64e42174c8adefbddf75a18cc105fa901a816be3
jesusninoc <jesusninoc@users.noreply.github.com>
2016-11-07 23:25:32 +0100

    Ayuda sobre la solución a las preguntas

---
+++ Examenes/Examen14-11-2017AyudaSoluciones.ps1
@@ -0,0 +1,46 @@
+#¿Qué recurso es el más valorado dentro del ordenador porque es escaso?.
+#HAZ UN INFORME SOBRE TODO LO VEAS QUE ESTÁ RELACIONADO CON EL RECURSO MÁS VALORADO.
+
+###########
+#Procesador
+#Memoria
+#Disco duro
+#E/S
+###########
+
+###########
+#Procesador
+###########
+
+#Get-Process | select *
+
+#https://msdn.microsoft.com/es-ES/library/system.diagnostics.process_properties.aspx
+
+#The default display of a process is a table that includes the following columns. For a description of all of the properties of process objects, see "Process Properties" on MSDN at http://go.microsoft.com/fwlink/?LinkId=204482.
+#-- Handles: The number of handles that the process has opened.
+#-- NPM(K): The amount of non-paged memory that the process is using, in kilobytes.
+#-- PM(K): The amount of pageable memory that the process is using, in kilobytes.
+#-- WS(K): The size of the working set of the process, in kilobytes. The working set consists of the pages of memory that were recently referenced by the process.
+#-- VM(M): The amount of virtual memory that the process is using, in megabytes. Virtual memory includes storage in the paging files on disk.
+#-- CPU(s): The amount of processor time that the process has used on all processors, in seconds.
+#-- ID: The process ID (PID) of the process.
+#-- ProcessName: The name of the process.
+#For explanations of the concepts related to processes, see the Glossary in Help and Support Center and the Help for Task Manager.
+
+###########
+#Memoria
+###########
+
+#http://www.jesusninoc.com/2016/10/22/3-gestion-del-hardware-en-powershell-para-administradores-de-sistemas/
+
+###########
+#Disco duro
+###########
+
+#http://www.jesusninoc.com/2016/10/22/3-gestion-del-hardware-en-powershell-para-administradores-de-sistemas/
+
+###########
+#E/S
+###########
+
+#http://www.jesusninoc.com/2016/10/22/3-gestion-del-hardware-en-powershell-para-administradores-de-sistemas/
