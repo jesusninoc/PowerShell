56e92c7a1383f00845dabd85e85d9bf37f5a9007
Jesús <jesnino@users.noreply.github.com>
2017-11-21 21:53:29 +0100

    Update and rename Detectar si hay que actualizar una BIOS to Detectar si hay que actualizar una BIOS.ps1

--- Seguridad/Detectar si hay que actualizar una BIOS.ps1
+++ Seguridad/Detectar si hay que actualizar una BIOS.ps1
@@ -1,7 +1,7 @@
 #Si la versión de la BIOS del equipo se encuentra dentro del fichero, hay que actualizarla
 
 #Fichero bios.txt contiene _ASUS_ - 1072009
-"_ASUS_ - 1072009" | Out-File bios.txt -Append
+#"_ASUS_ - 1072009" | Out-File bios.txt -Append
 
 gc .\bios.txt | %{
 if((Get-WmiObject Win32_BIOS).version -eq $_)
