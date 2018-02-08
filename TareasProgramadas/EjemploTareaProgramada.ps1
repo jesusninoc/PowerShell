133ba70f0a4e3c998caa60bb16aa0463fbefeb64
Jesús <16318863+jesusninoc@users.noreply.github.com>
2018-02-08 22:00:45 +0100

    Update EjemploTareaProgramada.ps1

--- TareasProgramadas/EjemploTareaProgramada.ps1
+++ TareasProgramadas/EjemploTareaProgramada.ps1
@@ -3,7 +3,7 @@ $action=New-ScheduledTaskAction -Execute 'Powershell.exe' -Argument '-NoProfile 
 $trigger=New-ScheduledTaskTrigger -Daily -At 9am
 Register-ScheduledTask -Action $action -Trigger $trigger -TaskName "TareaProgramada" -Description "Comprobación de valor"
 
-#Crear una tarea programada que ejecuta Notepa
+#Crear una tarea programada que ejecuta Notepad
 Register-ScheduledTask Task01 -Action (New-ScheduledTaskAction -Execute "notepad") -Principal (New-ScheduledTaskPrincipal -GroupId "BUILTIN\administradores" -RunLevel Highest) -Settings (New-ScheduledTaskSettingsSet -RestartCount 5 -RestartInterval 60)
 
 #Crear una tarea programada en un equipo remoto
