85b92f094d23a088cd89370e8ae37da9079d7769
Jesús <16318863+jesusninoc@users.noreply.github.com>
2021-02-08 13:40:31 +0100

    Update and rename RepasoBucleForEachInverso.ps1 to RepasoBucleForEachInverso.md

---
+++ Bucles/RepasoBucleForEachInverso.md
@@ -0,0 +1,1 @@
+* https://www.jesusninoc.com/02/08/ejercicios-de-powershell-mostrar-los-5-ultimos-procesos-que-se-estan-ejecutando-de-forma-descendente/
--- Bucles/RepasoBucleForEachInverso.ps1
+++
@@ -1,3 +0,0 @@
-#Mostrar los 5 últimos procesos que se están ejecutando de forma descendente
-
-for($i=(ps).count ; $i -gt ((ps).count)-5 ; $i=$i-1){(ps)[$i]}
