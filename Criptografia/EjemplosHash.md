1695030d10a40142c50060033e6e4cad878f1345
Jesús <16318863+jesusninoc@users.noreply.github.com>
2021-02-08 14:53:33 +0100

    Update and rename EjemplosHash.ps1 to EjemplosHash.md

---
+++ Criptografia/EjemplosHash.md
@@ -0,0 +1,1 @@
+* https://www.jesusninoc.com/02/08/ejercicios-de-powershell-obtener-md5-para-una-cadena/
--- Criptografia/EjemplosHash.ps1
+++
@@ -1,4 +0,0 @@
-#Obtener MD5 para una cadena
-[Reflection.Assembly]::LoadWithPartialName("System.Web")
-[System.Web.Security.FormsAuthentication]::HashPasswordForStoringInConfigFile("Hola", "MD5")
-[System.Web.Security.FormsAuthentication]::HashPasswordForStoringInConfigFile("hola", "MD5")
