7f6534d4d11b7f5ee2d6af609ee0bf0729fbea86
Jesús <16318863+jesusninoc@users.noreply.github.com>
2021-02-09 07:42:31 +0100

    Update and rename EjemplosFicheros.ps1 to EjemplosFicheros.md

---
+++ Ficheros/EjemplosFicheros.md
@@ -0,0 +1,1 @@
+* https://www.jesusninoc.com/02/09/ejercicios-de-powershell-listar-ficheros-con-get-childitem/
--- Ficheros/EjemplosFicheros.ps1
+++
@@ -1,6 +0,0 @@
-Get-ChildItem
-Get-ChildItem –Path *.txt -Recurse -Force
-Get-ChildItem -Name
-Get-ChildItem –Path C:\Windows\Logs\* -Include *.txt -Exclude A*
-Get-ChildItem –Path HKLM:\Software
-Get-ChildItem –Path C:\Windows –Include *mouse* -Exclude *.png
